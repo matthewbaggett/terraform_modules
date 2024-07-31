7e033ef6231c0b0dd4ed6d5ab6bc1ef1ff358e76
Matthew Baggett <matthew@baggett.me>
2024-07-31 14:46:25 +0200

    Add required_version

--- docker-gitea-actions-runner/terraform.tf
+++ docker-gitea-actions-runner/terraform.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "~> 1.6"
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
--- docker-postgres/terraform.tf
+++ docker-postgres/terraform.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "~> 1.6"
+
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"

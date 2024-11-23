79a514c1ac198b9d595cc20a048c272addc6d8b0
Matthew Baggett <matthew@baggett.me>
2024-11-23 17:21:54 +0100

    fix bug with magic forwarding.

--- products/unifi/magic-forward/magic_forward.tf
+++ products/unifi/magic-forward/magic_forward.tf
@@ -2,6 +2,6 @@ module "port_forward" {
   source  = "../port-forward"
   enabled = var.enabled
   label   = var.label
-  port_to = var.port != null ? var.port : var.docker_service.endpoint_spec[0].ports[0].published_port
+  port    = var.port != null ? var.port : var.docker_service.endpoint_spec[0].ports[0].published_port
   ip      = var.target.fixed_ip
 }

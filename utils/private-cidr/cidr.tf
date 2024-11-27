e58d33657e2fef9d050aedc18b6c842b08e3673d
Matthew Baggett <matthew@baggett.me>
2024-11-27 16:22:20 +0100

    I can do logic me

--- utils/private-cidr/cidr.tf
+++ utils/private-cidr/cidr.tf
@@ -8,7 +8,7 @@ resource "random_integer" "lower_mid_byte" {
 }
 locals {
   // Generate a subnet
-  subnet = var.override_subnet != null ? "172.${random_integer.upper_mid_byte.result}.${random_integer.lower_mid_byte.result}.0/${var.subnet_mask}" : ""
+  subnet = var.override_subnet != null ? var.override_subnet : "172.${random_integer.upper_mid_byte.result}.${random_integer.lower_mid_byte.result}.0/${var.subnet_mask}"
   // Calculate the gateway from the subnet
   gateway = cidrhost(local.subnet, 1)
 }

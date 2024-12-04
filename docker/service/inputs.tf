ed6cd2222dc3ae05e7f1a3143443f79387c3d26d
Matthew Baggett <matthew@baggett.me>
2024-12-04 14:30:45 +0100

    Support dynamic port allocation on host

--- docker/service/inputs.tf
+++ docker/service/inputs.tf
@@ -80,22 +80,25 @@ variable "configs" {
 }
 variable "ports" {
   type = list(object({
-    host      = number
+    host      = optional(number, null)
     container = number
     protocol  = optional(string, "tcp")
   }))
   default     = []
   description = "A map of port mappings to expose on the host. The key is the host port, and the value is the container port."
   validation {
-    error_message = "Host Ports must be between 1 and 65535."
-    condition     = alltrue([for port in var.ports : port.host >= 1 && port.host <= 65535])
+    error_message = "Host Ports must be between 1 and 65535. Or null."
+    # This condition is structured wierd because terraform doesn't have
+    # short-circuiting and a variable that is null cannot also be read
+    # for the port number validation.
+    condition = alltrue([for port in var.ports : port.host == null ? true : (port.host >= 1 && port.host <= 65535)])
   }
   validation {
     error_message = "Container Ports must be between 1 and 65535."
     condition     = alltrue([for port in var.ports : port.container >= 1 && port.container <= 65535])
   }
   validation {
-    error_message = "protocol must be either 'tcp' or 'udp'."
+    error_message = "Protocol must be either 'tcp' or 'udp'."
     condition     = alltrue([for port in var.ports : port.protocol == "tcp" || port.protocol == "udp"])
   }
 }

dacce7164556b4cf79a14d124c386afe2a98851f
Matthew Baggett <matthew@baggett.me>
2024-12-05 16:45:59 +0100

    docker socket proxy needs to be on a manager.

--- products/traefik/docker-socket-proxy.tf
+++ products/traefik/docker-socket-proxy.tf
@@ -1,5 +1,5 @@
 module "docker_socket_proxy" {
   source                = "../../docker/socket-proxy"
   stack_name            = var.stack_name
-  placement_constraints = var.placement_constraints
+  placement_constraints = concat(var.placement_constraints, ["node.role == manager"])
 }

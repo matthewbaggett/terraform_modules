f9c2c293150996947d14fdf777867aca9edbbb3c
Matthew Baggett <matthew@baggett.me>
2024-12-05 16:47:28 +0100

    Fmt

--- products/traefik/traefik.tf
+++ products/traefik/traefik.tf
@@ -37,15 +37,15 @@ module "traefik" {
     var.ssl_enable ? "--entrypoints.web.address=:80" : null,
 
     # Configure HTTPS
-      var.ssl_enable ? "--entrypoints.websecure.address=:443" : null,
-      var.ssl_enable && var.redirect_to_ssl ? "--entrypoints.web.http.redirections.entrypoint.to=websecure"  : null,
-      var.ssl_enable && var.redirect_to_ssl ? "--entrypoints.web.http.redirections.entrypoint.scheme=https"  : null,
+    var.ssl_enable ? "--entrypoints.websecure.address=:443" : null,
+    var.ssl_enable && var.redirect_to_ssl ? "--entrypoints.web.http.redirections.entrypoint.to=websecure" : null,
+    var.ssl_enable && var.redirect_to_ssl ? "--entrypoints.web.http.redirections.entrypoint.scheme=https" : null,
 
     # Configure the acme provider
-      var.ssl_enable ? "--certificatesresolvers.default.acme.tlschallenge=true" : null,
-      var.ssl_enable && var.acme_use_staging ? "--certificatesresolvers.default.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory"  : null,
-      var.ssl_enable ? "--certificatesresolvers.default.acme.email=${var.acme_email}" : null,
-      var.ssl_enable ? "--certificatesresolvers.default.acme.storage=/certs/acme.json" : null,
+    var.ssl_enable ? "--certificatesresolvers.default.acme.tlschallenge=true" : null,
+    var.ssl_enable && var.acme_use_staging ? "--certificatesresolvers.default.acme.caserver=https://acme-staging-v02.api.letsencrypt.org/directory" : null,
+    var.ssl_enable ? "--certificatesresolvers.default.acme.email=${var.acme_email}" : null,
+    var.ssl_enable ? "--certificatesresolvers.default.acme.storage=/certs/acme.json" : null,
   ])
   traefik = var.traefik_service_domain != null ? {
     domain = var.traefik_service_domain

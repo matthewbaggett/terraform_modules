e7935dd6811cde02d27f8f1c24a344914d909f19
Matthew Baggett <matthew@baggett.me>
2024-12-12 21:52:53 +0100

    Debug files should be json so that terraform can show nice deltas.

--- cloud/aws/s3_bucket/bucket.tf
+++ cloud/aws/s3_bucket/bucket.tf
@@ -4,8 +4,8 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "local_file" "debug" {
-  filename = "${path.root}/.debug/aws/s3_bucket/bucket.${aws_s3_bucket.bucket.bucket}.yml"
-  content = yamlencode({
+  filename = "${path.root}/.debug/aws/s3_bucket/bucket.${aws_s3_bucket.bucket.bucket}.json"
+  content = jsonencode({
     bucket_prefix = var.bucket_name_prefix,
     tags          = local.tags,
     endpoint      = aws_s3_bucket.bucket.bucket_domain_name
---
+++ docker/service/debug.tf
@@ -0,0 +1,16 @@
+resource "local_file" "debug" {
+  filename        = "${path.root}/.debug/docker/services/${local.service_name}.json"
+  file_permission = "0600"
+  content = jsonencode(nonsensitive({
+    name  = local.service_name
+    stack = var.stack_name
+    #image = local.image
+    build                 = var.build
+    networks              = var.networks
+    ports                 = var.ports
+    configs               = var.configs
+    traefik               = var.traefik
+    placement_constraints = var.placement_constraints
+    tags                  = local.tags
+  }))
+}

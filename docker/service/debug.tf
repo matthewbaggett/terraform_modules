20d3b53b01cc44044c1ba8015e526f303ba63833
Matthew Baggett <matthew@baggett.me>
2024-12-21 01:16:30 +0100

    really annoying to hide debug output...

--- docker/service/debug.tf
+++ docker/service/debug.tf
@@ -1,7 +1,7 @@
 resource "local_file" "debug" {
   filename        = "${path.root}/.debug/docker/services/${local.service_name}.json"
   file_permission = "0600"
-  content = jsonencode({
+  content = nonsensitive(jsonencode({
     name  = local.service_name
     stack = var.stack_name
     #image = local.image
@@ -12,5 +12,5 @@ resource "local_file" "debug" {
     traefik               = var.traefik
     placement_constraints = var.placement_constraints
     build_tags            = local.is_build ? local.tags : []
-  })
+  }))
 }

ad8b3aa54a90e1d83ea2ae02d78518460fe49a9f
Matthew Baggett <matthew@baggett.me>
2025-01-06 13:08:54 +0100

    Fix bug in mirroring images

--- docker/service/image.mirror.tf
+++ docker/service/image.mirror.tf
@@ -3,18 +3,18 @@ locals {
 }
 resource "docker_image" "source" {
   for_each      = local.is_mirror ? { "mirror" = {} } : {}
-  name          = data.docker_registry_image.image[0].name
-  pull_triggers = [data.docker_registry_image.image[0].sha256_digest]
+  name          = data.docker_registry_image.image["default"].name
+  pull_triggers = [data.docker_registry_image.image["default"].sha256_digest]
   force_remove  = false
 }
 resource "docker_tag" "retagged" {
   for_each     = local.is_mirror ? { "mirror" = {} } : {}
-  source_image = docker_image.source[0].name
+  source_image = docker_image.source["mirror"].name
   target_image = var.mirror
 }
 resource "docker_registry_image" "mirror" {
   for_each      = local.is_mirror ? { "mirror" = {} } : {}
-  depends_on    = [docker_tag.retagged[0]]
-  name          = docker_tag.retagged[0].target_image
+  depends_on    = [docker_tag.retagged["mirror"]]
+  name          = docker_tag.retagged["mirror"].target_image
   keep_remotely = true
 }

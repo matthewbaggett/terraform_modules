491dcc165136ec3d73d46bb3e98d8572a1fa6d57
Matthew Baggett <matthew@baggett.me>
2025-01-06 15:57:25 +0100

    Add validation that requires atleast one user specified

--- cloud/aws/s3_bucket/inputs.tf
+++ cloud/aws/s3_bucket/inputs.tf
@@ -17,4 +17,8 @@ variable "users" {
   type        = list(string)
   default     = []
   description = "List of users to generate S3 API keys for. Will be used as the IAM name."
+  validation {
+    condition     = length(var.users) > 0
+    error_message = "At least one user must be specified!"
+  }
 }

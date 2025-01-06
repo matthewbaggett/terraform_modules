2e41b36e53d684eeaeeb537e7e9388c65926fd50
Matthew Baggett <matthew@baggett.me>
2025-01-06 12:22:08 +0100

    fmt

--- products/dex/treafik-forward-auth.tf
+++ products/dex/treafik-forward-auth.tf
@@ -1,12 +1,12 @@
 resource "random_password" "forward_auth_secret" {
-  length = 32
+  length  = 32
   special = false
 }
 module "forward_auth" {
-  source = "../../docker/service"
-  stack_name = var.stack_name
+  source       = "../../docker/service"
+  stack_name   = var.stack_name
   service_name = "forward-auth"
-    image = "thomseddon/traefik-forward-auth"
+  image        = "thomseddon/traefik-forward-auth"
   traefik = {
     port = 4181
   }
@@ -15,7 +15,7 @@ module "forward_auth" {
     "traefik.http.middlewares.forward-auth.forwardauth.authResponseHeaders" = "X-Forwarded-User"
   }
   environment_variables = {
-    SECRET= random_password.forward_auth_secret.result
-    INSECURE_COOKIE=var.traefik.ssl ? "false" : "true"
+    SECRET          = random_password.forward_auth_secret.result
+    INSECURE_COOKIE = var.traefik.ssl ? "false" : "true"
   }
 }

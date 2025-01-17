50754e1ec9b9fe002fab0736a9de6df2916865bd
Matthew Baggett <matthew@baggett.me>
2025-01-17 12:19:57 +0100

    Improve headers support and treafik labelling

--- docker/service/traefik.tf
+++ docker/service/traefik.tf
@@ -9,6 +9,7 @@ variable "traefik" {
     middlewares      = optional(list(string), [])
     network          = optional(object({ name = string, id = string }))
     basic-auth-users = optional(list(string), [])
+    headers          = optional(map(string), {})
   })
   description = "Whether to enable traefik for the service."
 }
@@ -45,7 +46,18 @@ locals {
         ])
       }
       : {}
-    ) : {}
+    )
+    : {}
+  )
+  traefik_headers = (
+    local.is_traefik
+    ? { for key, value in var.traefik.headers : "traefik.http.middlewares.${local.traefik_service}-headers.headers.customrequestheaders.${key}" => value }
+    : {}
+  )
+  traefik_middlewares = concat(
+    try(var.traefik.middlewares, []),
+    length(local.traefik_basic_auth) > 0 ? ["${local.traefik_service}-auth"] : [],
+    length(local.traefik_headers) > 0 ? ["${local.traefik_service}-headers"] : []
   )
   has_auth = length(keys(local.traefik_basic_auth)) > 0
 
@@ -57,38 +69,36 @@ locals {
     ? (
       var.traefik.rule != null
       ? var.traefik.rule
-      : (
-        var.traefik.domain != null
-        ? "Host(`${var.traefik.domain}`)"
-        : null
-      )
-    ) : null
+      : "Host(`${var.traefik.domain}`)"
+    )
+    : null
+  )
+  traefik_entrypoints = compact(
+    local.is_traefik
+    ? [var.traefik.non-ssl ? "web" : null, var.traefik.ssl ? "websecure" : null]
+    : []
   )
   traefik_labels = (
     local.is_traefik
-    ? merge({
-      "traefik.enable"                                                             = "true"
-      "traefik.http.routers.${local.traefik_service}.rule"                         = var.traefik.non-ssl ? local.traefik_rule : null
-      "traefik.http.routers.${local.traefik_service}.service"                      = var.traefik.non-ssl ? local.traefik_service : null
-      "traefik.http.routers.${local.traefik_service}.entrypoints"                  = var.traefik.non-ssl ? "web" : null
-      "traefik.http.routers.${local.traefik_service}.tls"                          = var.traefik.non-ssl ? "" : null
-      "traefik.http.services.${local.traefik_service}.loadbalancer.passhostheader" = var.traefik.non-ssl ? "true" : null
-      "traefik.http.services.${local.traefik_service}.loadbalancer.server.port"    = var.traefik.non-ssl ? var.traefik.port : null
-
-      "traefik.http.routers.${local.traefik_service}-ssl.rule"                         = var.traefik.ssl ? local.traefik_rule : null
-      "traefik.http.routers.${local.traefik_service}-ssl.service"                      = var.traefik.ssl ? "${local.traefik_service}-ssl" : null
-      "traefik.http.routers.${local.traefik_service}-ssl.entrypoints"                  = var.traefik.ssl ? "websecure" : null
-      "traefik.http.routers.${local.traefik_service}-ssl.tls.certresolver"             = var.traefik.ssl ? "default" : null
-      "traefik.http.services.${local.traefik_service}-ssl.loadbalancer.passhostheader" = var.traefik.ssl ? "true" : null
-      "traefik.http.services.${local.traefik_service}-ssl.loadbalancer.server.port"    = var.traefik.ssl ? var.traefik.port : null
+    ? merge(
+      {
+        "traefik.enable"                                                             = "true"
+        "traefik.http.routers.${local.traefik_service}.rule"                         = local.traefik_rule
+        "traefik.http.routers.${local.traefik_service}.service"                      = local.traefik_service
+        "traefik.http.routers.${local.traefik_service}.entrypoints"                  = join(",", local.traefik_entrypoints)
+        "traefik.http.routers.${local.traefik_service}.tls.certresolver"             = var.traefik.ssl ? "default" : null
+        "traefik.http.services.${local.traefik_service}.loadbalancer.passhostheader" = "true"
+        "traefik.http.services.${local.traefik_service}.loadbalancer.server.port"    = var.traefik.port
+        "traefik.http.routers.${local.traefik_service}.middlewares"                  = length(local.traefik_middlewares) > 0 ? join(",", local.traefik_middlewares) : null
       },
       (local.traefik_middlewares != null
         ? {
-          "traefik.http.routers.${local.traefik_service}.middlewares"     = var.traefik.non-ssl ? (length(local.traefik_middlewares) > 0 ? join(",", local.traefik_middlewares) : null) : null
-          "traefik.http.routers.${local.traefik_service}-ssl.middlewares" = var.traefik.ssl ? (length(local.traefik_middlewares) > 0 ? join(",", local.traefik_middlewares) : null) : null
+          "traefik.http.routers.${local.traefik_service}.middlewares"     = var.traefik.non-ssl ? join(",", local.traefik_middlewares) : null
+          "traefik.http.routers.${local.traefik_service}-ssl.middlewares" = var.traefik.ssl ? join(",", local.traefik_middlewares) : null
         } : {}
       ),
       local.traefik_basic_auth,
+      local.traefik_headers,
   ) : {})
 }
 

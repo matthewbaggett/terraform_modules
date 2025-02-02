ad3cd2f14a6f90a68b92b897ecc6c8e0fb97b4a9
Matthew Baggett <matthew@baggett.me>
2025-02-02 19:02:14 +0100

    Fixup incorrectly booting postgres

--- products/postgres/postgres.tf
+++ products/postgres/postgres.tf
@@ -5,17 +5,21 @@ module "service" {
   stack_name           = var.stack_name
   service_name         = var.service_name
   networks             = var.networks
-  healthcheck          = ["CMD-SHELL", "pg_isready", "-U", local.username, "-d", local.database]
+  healthcheck          = ["CMD-SHELL", "pg_isready", "-d $${POSTGRES_DB}", "--host=${var.stack_name}-${var.service_name}", "-U $${POSTGRES_USER}", ]
   healthcheck_interval = "10s"
   environment_variables = {
+    PGUSER            = local.username
     POSTGRES_USER     = local.username
     POSTGRES_PASSWORD = local.password
     POSTGRES_DB       = local.database
+    PGDATA            = "/var/lib/postgres/data"
   }
   volumes               = local.volumes
   mounts                = local.mounts
   ports                 = var.ports
   placement_constraints = var.placement_constraints
+  parallelism           = 1
+  start_first           = false
 }
 locals {
   volumes = var.data_persist_path == null ? {

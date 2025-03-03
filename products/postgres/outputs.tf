136094da5bf7d6caf69daa0045045dcbe723266e
Matthew Baggett <matthew@baggett.me>
2025-03-03 14:35:08 +0100

    Fix bug with PGDATA location

--- products/postgres/outputs.tf
+++ products/postgres/outputs.tf
@@ -16,11 +16,9 @@ output "ports" {
 output "docker_service" {
   value = module.service.docker_service
 }
-
 output "endpoint" {
   value = "postgres://${local.username}:${local.password}@${module.service.service_name}:5432/${local.database}"
 }
-
 output "pgadmin_config" {
   value = {
     "Group"         = var.stack_name

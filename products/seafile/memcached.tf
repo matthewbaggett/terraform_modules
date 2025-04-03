f77dbaa04c9e379d41381f53266782f96bee9133
Matthew Baggett <matthew@baggett.me>
2025-04-03 16:37:11 +0200

    No. No seafile.

--- products/seafile/memcached.tf
+++
@@ -1,7 +0,0 @@
-module "memcached" {
-  source                = "../memcached"
-  enable                = var.enable
-  stack_name            = var.stack_name
-  networks              = [module.network]
-  placement_constraints = var.placement_constraints
-}
--- products/seafile/network.tf
+++
@@ -1,5 +0,0 @@
-module "network" {
-  source       = "../../docker/network"
-  stack_name   = var.stack_name
-  network_name = "nextcloud"
-}

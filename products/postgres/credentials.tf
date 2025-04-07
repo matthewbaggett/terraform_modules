eb93f2b61ec350ea4f276f4db10c0b11bcf8d687
Matthew Baggett <matthew@baggett.me>
2025-04-07 18:41:52 +0200

    Do not generate random_* when value is provided.

--- products/postgres/credentials.tf
+++ products/postgres/credentials.tf
@@ -1,15 +1,18 @@
 resource "random_pet" "username" {
+  count  = var.username != null ? 0 : 1
   length = 2
 }
 resource "random_password" "password" {
+  count   = var.password != null ? 0 : 1
   length  = 32
   special = false
 }
 resource "random_pet" "database" {
+  count  = var.database != null ? 0 : 1
   length = 2
 }
 locals {
-  username = var.username != null ? var.username : replace(random_pet.username.id, "-", "")
-  password = var.password != null ? var.password : nonsensitive(random_password.password.result)
-  database = var.database != null ? var.database : replace(random_pet.database.id, "-", "")
+  username = var.username != null ? var.username : replace(random_pet.username[0].id, "-", "")
+  password = var.password != null ? var.password : nonsensitive(random_password.password[0].result)
+  database = var.database != null ? var.database : replace(random_pet.database[0].id, "-", "")
 }

5b1b002041f0569c601cce1d718bb2714b2fa4f6
Matthew Baggett <matthew@baggett.me>
2025-04-07 18:41:35 +0200

    output credentials

--- products/portainer/ui/output.tf
+++ products/portainer/ui/output.tf
@@ -1,14 +1,20 @@
+locals {
+  credentials = {
+    username = "admin" # Sorry, this is hardcoded in the portainer image
+    password = nonsensitive(random_password.password.result)
+  }
+}
 output "agent" {
   value = {
-    credentials = {
-      username = "admin" # Sorry, this is hardcoded in the portainer image
-      password = nonsensitive(random_password.password.result)
-    }
+    credentials       = local.credentials
     portainer_version = var.portainer_version
     service_name      = module.portainer.docker_service.name
     network           = module.network
     stack_name        = var.stack_name
   }
+}
+output "credentials" {
+  value = local.credentials
 }
 output "network" {
   value = module.network

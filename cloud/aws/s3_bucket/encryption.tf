add971e09da5a59300cf713431cfa600e8953808
Matthew Baggett <matthew@baggett.me>
2025-04-15 16:34:54 +0200

    Linting fixups

--- cloud/aws/s3_bucket/encryption.tf
+++ cloud/aws/s3_bucket/encryption.tf
@@ -1,5 +1,5 @@
 resource "aws_kms_key" "bucket" {
-  description = "Used to encrypt S3 bucket ${aws_s3_bucket.bucket.bucket}"
+  description             = "Used to encrypt S3 bucket ${aws_s3_bucket.bucket.bucket}"
   deletion_window_in_days = 10
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
   rule {
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.bucket.id
-      sse_algorithm = "aws:kms"
+      sse_algorithm     = "aws:kms"
     }
   }
 }

17e0c7f0e9c4bf7f16ee63f7e89124089228f9d5
Matthew Baggett <matthew@baggett.me>
2025-05-11 17:04:06 +0200

    Remove unsupported products

--- products/dex/postgres.tf
+++
@@ -1,6 +0,0 @@
-module "postgres" {
-  source     = "../../products/postgres"
-  stack_name = var.stack_name
-  database   = "dex"
-  networks   = [module.network]
-}
--- products/headscale/network.tf
+++
@@ -1,4 +0,0 @@
-module "network" {
-  source     = "../../docker/network"
-  stack_name = var.stack_name
-}

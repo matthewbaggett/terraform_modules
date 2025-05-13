28f963ab96da1dc423b7d2309ebf9444c8f4d0f7
Matthew Baggett <matthew@baggett.me>
2025-05-13 20:51:50 +0200

    Whitespace

--- cloud/aws/rds/serverless/locals.tf
+++ cloud/aws/rds/serverless/locals.tf
@@ -1,7 +1,12 @@
 locals {
-  supported_engines  = ["aurora-mysql", "aurora-postgresql", ]
-  ca_cert_identifier = aws_rds_cluster_instance.instance.ca_cert_identifier
-  debug_path         = "${path.root}/.debug/aws/rds/serverless/${var.instance_name}"
+  supported_engines = {
+    mysql    = ["mysql", "aurora-mysql", ]
+    mariadb  = ["mariadb", ]
+    postgres = ["postgresql", "aurora-postgresql", ]
+  }
+  supported_engines_list = flatten([for k, v in local.supported_engines : v])
+  ca_cert_identifier     = aws_rds_cluster_instance.instance.ca_cert_identifier
+  debug_path             = "${path.root}/.debug/aws/rds/serverless/${var.instance_name}"
   endpoints = {
     write = {
       # Host should be the same as the cluster endpoint, sans the port
